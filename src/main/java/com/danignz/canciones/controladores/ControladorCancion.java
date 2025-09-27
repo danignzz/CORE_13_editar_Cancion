9cd7b03b400fbaca0702bdb2d5ba26f63b8a3879
danignzz <danielagnz2003@gmail.com>
2025-09-27 02:39:32 -0300

    se agrego el metodo DeleteMapping

--- src/main/java/com/danignz/canciones/controladores/ControladorCancion.java
+++ src/main/java/com/danignz/canciones/controladores/ControladorCancion.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -80,4 +81,12 @@ public String formularioEditarCancion(@PathVariable Long idCancion, Model model)
     servicioCancion.actualizaCancion(cancion);
     return "redirect:/canciones";
     }
+
+    @DeleteMapping("/eliminar/cancion/{idCancion}")
+   	public String eliminarCancion(@PathVariable("idCancion") Long idCancion
+		) {
+		this.servicioCancion.eliminarCancion(idCancion);
+
+     	  	return "redirect:/canciones";
+   	}
 }
