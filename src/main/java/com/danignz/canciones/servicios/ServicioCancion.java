bc6ebdb6d274d8741de92d4c6e8abd2611261b9a
danignzz <danielagnz2003@gmail.com>
2025-09-27 00:56:35 -0300

    Se agrega nuevo metodo

---
+++ src/main/java/com/danignz/canciones/servicios/ServicioCancion.java
@@ -0,0 +1,39 @@
+package com.danignz.canciones.servicios;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.danignz.canciones.modelos.Cancion;
+import com.danignz.canciones.repositorios.RepositorioCancion;
+
+@Service
+public class ServicioCancion {
+    
+    @Autowired
+    private final RepositorioCancion repositorioCancion;
+
+    public ServicioCancion(RepositorioCancion repositorioCancion){
+        this.repositorioCancion = repositorioCancion;     
+    }
+
+    public List<Cancion> obtenerTodasLasCanciones() {
+    List<Cancion> lista = new ArrayList<>();
+    repositorioCancion.findAll().forEach(lista::add);
+    return lista;
+    }
+
+    public Cancion obtenerCancionPorId(Long id){
+        return this.repositorioCancion.findById(id).orElse(null);
+    }
+
+    public Cancion agregarCancion(Cancion cancion){
+        return repositorioCancion.save(cancion);
+    }
+
+    public Cancion actualizaCancion(Cancion cancion){
+        return repositorioCancion.save(cancion);
+    }
+}
